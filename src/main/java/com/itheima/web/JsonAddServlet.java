4b499989764d44c91fe0b8c03e506f423346b637
liuyx599 <995066544@qq.com>
2023-03-25 20:53:57 +0800

    # add Json

---
+++ src/main/java/com/itheima/web/JsonAddServlet.java
@@ -0,0 +1,46 @@
+package com.itheima.web;
+
+import com.alibaba.fastjson.JSON;
+import com.itheima.pojo.Brand;
+import com.itheima.service.BrandService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@WebServlet("/jsonAddServlet")
+public class JsonAddServlet extends HttpServlet {
+
+    private final BrandService brandService = new BrandService();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //1. 接收数据,request.getParameter 不能接收json的数据
+       /* String brandName = request.getParameter("brandName");
+        System.out.println(brandName);*/
+
+        // 获取请求体数据
+        BufferedReader br = request.getReader();
+        String params = br.readLine();
+
+        // 将JSON字符串转为Java对象
+        Brand brand = JSON.parseObject(params, Brand.class);
+
+
+        //2. 调用service 添加
+        brandService.add(brand);
+
+        //3. 响应成功标识
+        response.getWriter().write("success");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
---
+++ src/main/java/com/itheima/web/JsonSelectAllServlet.java
@@ -0,0 +1,36 @@
+package com.itheima.web;
+
+import com.alibaba.fastjson.JSON;
+import com.itheima.pojo.Brand;
+import com.itheima.service.BrandService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/jsonSelectAllServlet")
+public class JsonSelectAllServlet extends HttpServlet {
+    private final BrandService brandService = new BrandService();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1. 调用Service查询
+        List<Brand> brands = brandService.selectAll();
+
+        //2. 将集合转换为JSON数据   序列化
+        String jsonString = JSON.toJSONString(brands);
+
+        //3. 响应数据
+        response.setContentType("text/json;charset=utf-8");
+        response.getWriter().write(jsonString);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
