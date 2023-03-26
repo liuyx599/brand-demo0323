2d199eee5f08a132d8cabb9891348e3cb823a165
liuyx599 <995066544@qq.com>
2023-03-26 15:03:11 +0800

    #  添加vue  brand.html & addBrand.html

--- src/main/java/com/itheima/web/AddServlet.java
+++ src/main/java/com/itheima/web/AddServlet.java
@@ -1,46 +1,42 @@
 package com.itheima.web;
 
+import com.alibaba.fastjson.JSON;
 import com.itheima.pojo.Brand;
 import com.itheima.service.BrandService;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 @WebServlet("/addServlet")
 public class AddServlet extends HttpServlet {
-    private BrandService service = new BrandService();
 
+    private final BrandService brandService = new BrandService();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        //处理POST请求的乱码问题
-        request.setCharacterEncoding("utf-8");
+        //1. 接收数据,request.getParameter 不能接收json的数据
+       /* String brandName = request.getParameter("brandName");
+        System.out.println(brandName);*/
 
-        //1. 接收表单提交的数据，封装为一个Brand对象
-        String brandName = request.getParameter("brandName");
-        String companyName = request.getParameter("companyName");
-        String ordered = request.getParameter("ordered");
-        String description = request.getParameter("description");
-        String status = request.getParameter("status");
+        // 获取请求体数据
+        BufferedReader br = request.getReader();
+        String params = br.readLine();
 
-        //封装为一个Brand对象
-        Brand brand = new Brand();
-        brand.setBrandName(brandName);
-        brand.setCompanyName(companyName);
-        brand.setOrdered(Integer.parseInt(ordered));
-        brand.setDescription(description);
-        brand.setStatus(Integer.parseInt(status));
+        // 将JSON字符串转为Java对象
+        Brand brand = JSON.parseObject(params, Brand.class);
 
 
-        //2. 调用service 完成添加
-        service.add(brand);
+        //2. 调用service 添加
+        brandService.add(brand);
 
-
-        //3. 转发到查询所有Servlet
-        request.getRequestDispatcher("/selectAllServlet").forward(request,response);
+        //3. 响应成功标识
+        response.getWriter().write("success");
     }
 
     @Override
--- src/main/java/com/itheima/web/SelectAllServlet.java
+++ src/main/java/com/itheima/web/SelectAllServlet.java
@@ -1,30 +1,32 @@
 package com.itheima.web;
 
+import com.alibaba.fastjson.JSON;
 import com.itheima.pojo.Brand;
 import com.itheima.service.BrandService;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet("/selectAllServlet")
 public class SelectAllServlet extends HttpServlet {
-    private  BrandService service = new BrandService();
+    private final BrandService brandService = new BrandService();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1. 调用Service查询
+        List<Brand> brands = brandService.selectAll();
 
+        //2. 将集合转换为JSON数据   序列化
+        String jsonString = JSON.toJSONString(brands);
 
-        //1. 调用BrandService完成查询
-        List<Brand> brands = service.selectAll();
-
-        //2. 存入request域中
-        request.setAttribute("brands",brands);
-
-        //3. 转发到brand.jsp
-        request.getRequestDispatcher("/brand.jsp").forward(request,response);
+        //3. 响应数据
+        response.setContentType("text/json;charset=utf-8");
+        response.getWriter().write(jsonString);
     }
 
     @Override
